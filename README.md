9b90f5b0bdc72ce8672b5f03cfcdb48b8143841b
ekinadlim <eadlim@student.42vienna.com>
2025-08-28 15:22:12 +0200

    Added multiple headerfiles for each of their purpose

--- README.md
+++
@@ -1,6 +0,0 @@
-# cub3d
-A 42 Project, a Wolfenstein-like game
-
-Create new branch: git checkout -b [branch name]
-
-Switching branches: git checkout [branch name]
---
+++ structures.h
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structures.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eadlim <eadlim@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/28 15:16:19 by eadlim            #+#    #+#             */
+/*   Updated: 2025/08/28 15:17:12 by eadlim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCTURES_H
+# define STRUCTURES_H
+
+typedef struct s_player
+{
+	double	y;
+	double	x;
+	double	direction;
+}	t_player;
+
+typedef struct s_image
+{
+	void	*buffer;
+	char	*address;
+	int		bits_per_pixel;
+	int		size_line;
+	int		endian;
+}	t_image;
+
+typedef struct s_ray
+{
+	double	direction;
+	double	radian; //better name?
+	double	y_vector;
+	double	x_vector;
+	double	y;
+	double	x;
+	//double	step;
+	//bool	hit_wall;
+}	t_ray;
+
+typedef struct s_texture
+{
+	char	*north;
+	char	*south;
+	char	*west;
+	char	*east;
+
+}	t_texture;
+
+typedef struct s_color
+{
+	int	floor;
+	int	ceiling;
+}	t_color;
+
+typedef struct s_data
+{
+	void		*mlx;
+	void		*win;
+	int			win_height;
+	int			win_width;
+	t_player	player;
+	t_image		image;
+	t_ray		ray;
+	bool		keys[256];
+	long		time_reference;
+	char		**map;
+}	t_data;
+
+#endif // STRUCTURES_H
