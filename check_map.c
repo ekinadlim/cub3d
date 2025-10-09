99f1b39681f5b767682c861808838eaad850fd21
ekinadlim <eadlim@student.42vienna.com>
2025-10-09 16:50:02 +0200

    Removed floodfill function

--- check_map.c
+++ check_map.c
@@ -6,7 +6,7 @@
 /*   By: eadlim <eadlim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/24 13:43:38 by eadlim            #+#    #+#             */
-/*   Updated: 2025/09/30 15:40:26 by eadlim           ###   ########.fr       */
+/*   Updated: 2025/10/09 16:24:54 by eadlim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,41 +47,4 @@ void	check_map(char **map, t_data *data)
 		pos.x = 0;
 		pos.y++;
 	}
-}
-
-// Check for open walls;
-void	floodfill(t_vec_2d_int pos, char **map, t_data *data)
-{
-	/* 	for (size_t i = 0; i < (size_t)data->map.height; i++)
-		{
-			for (size_t j = 0; j < (size_t)data->map.width; j++)
-			{
-				if (j == x && i == y)
-					printf("P");
-				else
-					printf("%c", map[i][j]);
-			}
-			printf("\n");
-		}
-		printf("\n");
-		usleep(100000); */
-	if (pos.x == 0 || pos.y == 0)
-		exit_pars("We got contact with the void!", data);
-	if (map[pos.y + 1][pos.x] == ' ' || map[pos.y + 1][pos.x] == '\0')
-		exit_pars("We got contact with the void!", data);
-	if (map[pos.y - 1][pos.x] == ' ' || map[pos.y - 1][pos.x] == '\0')
-		exit_pars("We got contact with the void!", data);
-	if (map[pos.y][pos.x + 1] == ' ' || map[pos.y][pos.x + 1] == '\0')
-		exit_pars("We got contact with the void!", data);
-	if (map[pos.y][pos.x - 1] == ' ' || map[pos.y][pos.x - 1] == '\0')
-		exit_pars("We got contact with the void!", data);
-	map[pos.y][pos.x] = 'X';
-	if (map[pos.y + 1][pos.x] == '0')
-		floodfill((t_vec_2d_int){pos.x, pos.y + 1}, map, data);
-	if (map[pos.y - 1][pos.x] == '0')
-		floodfill((t_vec_2d_int){pos.x, pos.y - 1}, map, data);
-	if (map[pos.y][pos.x + 1] == '0')
-		floodfill((t_vec_2d_int){pos.x + 1, pos.y}, map, data);
-	if (map[pos.y][pos.x - 1] == '0')
-		floodfill((t_vec_2d_int){pos.x - 1, pos.y}, map, data);
 }
