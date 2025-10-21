d08b92588ee43e82c74fbaf0db468271f47ad30e
Armin <176120082+ArminPar@users.noreply.github.com>
2025-10-21 19:54:27 +0200

    removed for loop

--- main.c
+++ main.c
@@ -14,12 +14,14 @@
 
 void	update_animation_frame(t_data *data)
 {
+	int	i;
 	/* static long blabla = 0;
 	if (blabla == 0)
 	{
 		blabla = get_current_time();
 	} */
-	for (int i = 0; i < 4; i++)
+	i = 0;
+	while (i < 4)
 	{
 		data->animation[i].tracker += data->delta_time * ANIMATION_SPEED;
 		if ((int)data->animation[i].tracker)
@@ -27,8 +29,10 @@ void	update_animation_frame(t_data *data)
 			data->animation[i].index += (int)data->animation[i].tracker;
 			while (data->animation[i].index >= data->animation[i].frame_amount)
 				data->animation[i].index -= data->animation[i].frame_amount;
-			data->animation[i].tracker = data->animation[i].tracker - (int)data->animation[i].tracker;
+			data->animation[i].tracker
+				= data->animation[i].tracker - (int)data->animation[i].tracker;
 		}
+		i++;
 	}
 	//printf("%ld: Animation: frame %d, delta_time: %.4f\n", get_current_time() - blabla, data->animation.index[0], data->delta_time);
 }
@@ -62,36 +66,6 @@ int	game_loop(t_data *data)
 	return (0);
 }
 
-/* void	load_animations(t_data *data)
-{
-	//Just to test
-	char path[] = "textures/eyeNO/0.xpm";
-	for (int j = 0; j < 4; j++)
-	{
-		if (j == 1)
-			path[12] = 'E', path[13] = 'A';
-		else if (j == 2)
-			path[12] = 'S', path[13] = 'O';
-		else if (j == 3)
-			path[12] = 'W', path[13] = 'E';
-		path[15] = '0';
-		for (int i = 0; i < 10; i++)
-		{
-			if (data->animation.frame_amount[j] >= MAX_ANIMATION_FRAMES) //???
-				exit_cub3d("3AAAAAAAAAAAA");
-			data->animation[j][i].buffer = mlx_xpm_file_to_image(data->mlx, path, &data->animation[j][i].width, &data->animation[j][i].height);
-			if (!data->animation[j][i].buffer)
-				exit_cub3d("1AAAAAAAAAAAA");
-			data->animation[j][i].address = mlx_get_data_addr(data->animation[j][i].buffer, &data->animation[j][i].bytes_per_pixel, &data->animation[j][i].size_line, &data->animation[j][i].endian);
-			if (!data->animation[j][i].address)
-				exit_cub3d("2AAAAAAAAAAAA");
-			data->animation[j][i].bytes_per_pixel /= 8;
-			data->animation.frame_amount[j]++;
-			path[15]++;
-		}
-	}
-} */
-
 int	main(int argc, char **argv)
 {
 	t_data	*data;
@@ -118,7 +92,6 @@ int	main(int argc, char **argv)
 
 	calculate_fixed_values(data);
 	start_mlx(data);
-	//render_game(data); ???
 	mlx_loop(data->mlx);
 	return (0);
 }
